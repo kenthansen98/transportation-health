f99d273a33375a0c3b0a0b226a2bdae93c0cf85f
Kent Hansen <kenthansen98@gmail.com>
2022-03-31 21:52:22 -0500

    front page with search bar

---
+++ components/Search.tsx
@@ -0,0 +1,42 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const Search = () => {
+    const [searchText, setSearchText] = useState("");
+    const router = useRouter();
+
+    return (
+        <div className="self-center my-48 w-3/4 text-center">
+            <h3 className="text-lg text-slate-900 font-mono">
+                Find transportation and health data for your city:{" "}
+            </h3>
+            <form
+                className="flex flex-row place-content-center pt-8"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    if (searchText.length > 0) {
+                        router.push({
+                            pathname: "/search",
+                            query: { searchTerm: searchText },
+                        });
+                    }
+                }}
+            >
+                <input
+                    className="shadow-md p-2 rounded-lg mx-6 focus:outline-none focus:ring focus:ring-blue-600 w-1/2 transition focus:scale-105"
+                    value={searchText}
+                    onChange={(e) => setSearchText(e.target.value)}
+                    placeholder="Enter search"
+                />
+                <button
+                    className="bg-gradient-to-r from-blue-600 to-blue-500 p-2 rounded-lg shadow-md text-white hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600"
+                    type="submit"
+                >
+                    Search
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default Search;
